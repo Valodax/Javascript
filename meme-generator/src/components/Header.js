e66dd8d4b20a6e0001d461daf4fee3f526772850
Valodax <48880504+Valodax@users.noreply.github.com>
2022-05-26 00:54:41 +0200

    meme_generator

---
+++ meme-generator/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img
+                src="https://assets.stickpng.com/images/580b585b2edbce24c47b2a2a.png"
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
